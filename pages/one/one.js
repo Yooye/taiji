eea92bb4eca1af1f84cfaaf15825d69eb4ca860c
yooye <597665748@qq.com>
2019-03-07 20:19:32 +0800

    delete some useless code from the one.js

--- pages/one/one.js
+++ pages/one/one.js
@@ -59,32 +59,9 @@ Page({
     context.fill()
     context.closePath()
 
-    //贝塞尔曲线黑色小半圆
-    // 1，贝塞尔曲线值分析：
-    //【记录1】当时间段为12小时(720分钟)的贝塞尔曲线，context.bezierCurveTo(150, (150 + h)+100, 150 - r, (150 + h)+100, 150-r, 150+h)
-    //【记录2】当时间段为360分钟的贝塞尔曲线，(150 - 30, (150 + h) + 25, (150 - r) + 30, (150 + h) + 25, 150 - r, 150 + h)
-
-    // 2，结论：
-    // （1）起始弧度deg从 0 增大至 Math.PI/4;
-          // r从150减少至0
-          // h从0增大至150
-    // (2)黑色贝塞尔曲线的右侧控制点，逐渐向左下方移动：
-            //  x坐标变化  150 ~ 150-30
-            // y坐标增大了 100 ~ 25
-
-    // (3)黑色贝塞尔曲线的左侧控制点，逐渐向右下方移动：
-            //  x坐标增加了30
-            // y坐标增大了25
-
-      //（4）deg每次变化引发的贝塞尔曲线变化为：
-      // deg  /  Math.PI/2 = cX? / 60
-      
-
-    // var cX = deg < Math.PI / 4 ? 100 * (deg / Math.PI) : 20 * (deg / Math.PI);
-    // var cY = deg < Math.PI / 4 ? 300 * (deg / Math.PI) : 10 * (deg / Math.PI);
-    var cX = 10 * r / 100;
-    var cY;
-    console.log(deg)
+    // 基于小圆的左右两个点确定贝塞尔曲线的控制点变量
+    var cX = 10 * r / 100;  //贝塞尔曲线控制点x坐标偏移量
+    var cY;  //贝塞尔曲线控制点y坐标偏移量
     if(tim<=720&&tim>=600){
       cY = 60;
       console.log(111)
@@ -116,43 +93,43 @@ Page({
     context.draw()
   },
 
-  caculateTime:function(context,t){ //根据时间段，算出最终需要的红色不规则区域弧度
-      // const context = wx.createCanvasContext('firstCanvas');
-      var tim = t || 400;
-      // 如果用户时间段按整分钟进行划分，一个圆将被分为24小时，也就是24*60分钟，每一分钟的度数为：360/24*60，弧度为：2*Math.PI/24*60
+  // caculateTime:function(context,t){ //根据时间段，算出最终需要的红色不规则区域弧度
+  //     // const context = wx.createCanvasContext('firstCanvas');
+  //     var tim = t || 400;
+  //     // 如果用户时间段按整分钟进行划分，一个圆将被分为24小时，也就是24*60分钟，每一分钟的度数为：360/24*60，弧度为：2*Math.PI/24*60
 
-      var redDeg = tim * ((2 * Math.PI) / (24 * 60));
+  //     var redDeg = tim * ((2 * Math.PI) / (24 * 60));
 
-      // 如果我要先画一个正面朝上的红色半圆，该半圆的起始弧度设为deg
-      let deg = (Math.PI - redDeg) / 2; //绘制不规则圆的起始弧度
-      let R = 150; //大圆半径
-      // //小圆直径如下
-      var r = Math.sin(deg * Math.PI / 180) * R;
-      // //大圆圆心到底边距离 
-      var h = Math.cos(deg * Math.PI / 180) * R;
-      this.setData({
-        changer: r,
-        changey: 150 + h,
-        blackX: 150 - r / 2,
-        redX: 150 + r / 2
-      })
+  //     // 如果我要先画一个正面朝上的红色半圆，该半圆的起始弧度设为deg
+  //     let deg = (Math.PI - redDeg) / 2; //绘制不规则圆的起始弧度
+  //     let R = 150; //大圆半径
+  //     // //小圆直径如下
+  //     var r = Math.sin(deg * Math.PI / 180) * R;
+  //     // //大圆圆心到底边距离 
+  //     var h = Math.cos(deg * Math.PI / 180) * R;
+  //     this.setData({
+  //       changer: r,
+  //       changey: 150 + h,
+  //       blackX: 150 - r / 2,
+  //       redX: 150 + r / 2
+  //     })
 
-      context.beginPath()
-      console.log(deg,redDeg)
-      // context.moveTo(150, 150)
-      context.arc(150, 150, 150, deg, Math.PI-deg, true)
-      context.setFillStyle('#000');
-      context.fill();
-      context.closePath()
+  //     context.beginPath()
+  //     console.log(deg,redDeg)
+  //     // context.moveTo(150, 150)
+  //     context.arc(150, 150, 150, deg, Math.PI-deg, true)
+  //     context.setFillStyle('#000');
+  //     context.fill();
+  //     context.closePath()
 
-      context.beginPath()
-      // context.moveTo(150, 150)
-      context.arc(150, 150, 150, deg, Math.PI-deg, false)
-      context.setFillStyle('red');
-      context.fill()
-      context.closePath()
+  //     context.beginPath()
+  //     // context.moveTo(150, 150)
+  //     context.arc(150, 150, 150, deg, Math.PI-deg, false)
+  //     context.setFillStyle('red');
+  //     context.fill()
+  //     context.closePath()
 
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面显示
